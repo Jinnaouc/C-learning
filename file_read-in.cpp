1f2179c43cf7a40983ba1b5000f005bed65151e5
Jinna <Jinna_ouc@163.com>
2016-09-12 11:37:45 +0800

    add note of codes

--- file_read-in.cpp
+++ file_read-in.cpp
@@ -1,10 +1,10 @@
     ifstream data_file;
-    data_file.open("data.txt");
+    data_file.open("data.txt");// read in the data in data.txt
     for (int i = 0; i<data_number; i++)
     {
         for (int j = 0; j<data_dimension; j++)
         {
-            data_file >> inp.input_data[i][j];
+            data_file >> inp.input_data[i][j];//read the data into input_data array
         }
     }
     data_file.close();
